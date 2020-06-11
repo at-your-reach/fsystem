dda7b4d3a179ca7443d67b27b44e88672091f758
Your Name <you@example.com>
2020-06-10 22:10:10 -0700

    changes made in officer table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'customers#index'
   resources :customers
+  root 'officers#index'
   resources :officers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
