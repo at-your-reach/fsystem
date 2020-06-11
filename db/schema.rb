c90025622b74bb31dfe7701c73f381dbcdafb993
Your Name <you@example.com>
2020-06-10 20:31:02 -0700

    changes made in customer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_014505) do
+ActiveRecord::Schema.define(version: 2020_06_11_012332) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "other_name"
+    t.string "string"
+    t.date "birth_date"
+    t.integer "age"
+    t.string "gender"
+    t.string "marital_status"
+    t.string "nationality"
+    t.string "beneficiary"
+    t.string "mobile_number"
+    t.string "house_number"
+    t.string "landmark_location"
+    t.string "street_name"
+    t.string "region"
+    t.string "district"
+    t.string "town"
+    t.string "occupation"
+    t.string "id_type"
+    t.string "id_number"
+    t.string "spouse_name"
+    t.integer "number_of_children"
+    t.string "email"
+    t.date "date_account_opened"
+    t.integer "accounts_opened"
+    t.string "picture"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "officers", force: :cascade do |t|
     t.string "name"
