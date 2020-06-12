8e15c96d1ee36b4ac3c78c54a242af510c1376ae
Your Name <you@example.com>
2020-06-11 18:10:40 -0700

    changes made on th routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'customers#index'
-  resources :customers
-  root 'officers#index'
+  resources :customer
   resources :officers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
