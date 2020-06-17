58aa2ed6cc7ff70a0be771bdd2875e8e452b6c60
atyourr3ach <atyourr3ach@gmail.com>
2020-06-17 01:56:38 +0000

    Fixing error with webpacker config.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'customers#index'
   resources :customer
   resources :officers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
