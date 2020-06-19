1d8b8e746654103ae5b977270ac4e7b254ab20eb
atyourr3ach <atyourr3ach@gmail.com>
2020-06-19 01:56:03 +0000

    Config changes to fix 500 error in production.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ruby:2.6.6
-RUN apt-get update -qq && apt-get install -y nodejs postgresql-client vim
+RUN apt-get update -qq && apt-get install -y nodejs postgresql-client
 RUN mkdir /myapp
 WORKDIR /myapp
 COPY Gemfile /myapp/Gemfile
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
 	# config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 	config.hosts << "fsystem.herokuapp.com"
+	config.serve_static_files = true
 end
