04361bc4300fcf30c9e5af9f854f97a1529a4e73
atyourr3ach <atyourr3ach@gmail.com>
2020-06-20 09:12:33 +0000

    Small change in the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'customers#index'
-  resources :customer
+  resources :customers
   resources :officers
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
