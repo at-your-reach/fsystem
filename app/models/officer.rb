aff35984cd6fbba44213ece77b6f8e91ec6dc63e
atyourr3ach <atyourr3ach@gmail.com>
2020-06-23 03:37:18 +0000

    Working in the validations.

--- app/models/officer.rb
+++ app/models/officer.rb
@@ -1,2 +1,4 @@
 class Officer < ApplicationRecord
+  validates :name, :nationality, :mobile_number, :address, presence: true
+	validates :age, numericality: { only_integer: true, greater_than: 17 }
 end
