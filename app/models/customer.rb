5d80fc198b9098a3e4541b40c61e1f5f686ff127
atyourr3ach <atyourr3ach@gmail.com>
2020-06-23 03:44:45 +0000

    Fixing small error.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
 	validates :first_name, :last_name, :mobile_number, :house_number, :landmark_location, :id_type, :id_number, :town, presence: true
 	validates :age, numericality: { only_integer: true, greater_than: 17 }
-	validates :number_of_children, numericality: { only_integer: true, greater_than: 17 }
+	validates :number_of_children, numericality: { only_integer: true, greater_than: -1 }
 end
